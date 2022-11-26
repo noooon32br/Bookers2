bdb9749e28ba82e8175afdec96d97f9d5f4d45f1
EC2 Default User <ec2-user@ip-172-31-26-236.ap-northeast-1.compute.internal>
2022-11-26 12:33:11 +0000

    RSpectest

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
     if @user == current_user
       render "edit"
     else
-      redirect_to user_path(@user.id)
+      redirect_to user_path(current_user)
     end
   end
 
@@ -20,7 +20,6 @@ class UsersController < ApplicationController
     @books = @user.books
     @users = User.all
     @book = Book.new
-
   end
 
   def create
@@ -37,22 +36,22 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "You have updated user successfully."
-      redirect_to user_path(@user)
+      redirect_to user_path(@user.id)
     else
       render :edit
     end
   end
 
    private
-   
+
   def currect_user
     @book = Book.find(params[:id])
     @user = @book.user
     redirect_to(books_path) unless @user == current_user
   end
-   
-   
+
+
   def user_params
-    params.require(:user).permit(:Introduction, :name, :image)
+    params.require(:user).permit(:introduction, :name, :profile_image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'top' => 'homes#top'
-  get '/homes/about' => 'homes#about', as: 'about'
+  get '/home/about' => 'homes#about', as: 'about'
   resources :books, only: [:new, :create, :show, :index, :edit, :destroy, :update]
   resources :users, only: [:show, :edit, :update, :index, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
